47077973eae89ceed4c49f6b5b4c7c8e671c2510
think <310445479@qq.com>
2018-11-23 20:18:23 -0500

    add another gpu setting

--- src/search_decoder.py
+++ src/search_decoder.py
@@ -44,6 +44,7 @@ class GreedySearchDecoder(nn.Module):
             # Transform speaker_id from int into tensor with shape=(1, 1)
             speaker_id = torch.LongTensor([self.speaker])
             speaker_id = torch.unsqueeze(speaker_id, 1)
+            speaker_id = speaker_id.to(device)
             decoder_output, decoder_hidden = self.decoder(decoder_input, speaker_id, decoder_hidden, encoder_outputs)
             # Obtain most likely word token and its softmax score
             decoder_scores, decoder_input = torch.max(decoder_output, dim=1)
