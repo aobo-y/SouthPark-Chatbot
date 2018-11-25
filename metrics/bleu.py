79fdccff2c5afc7620d2de5d7014d1eaea320c78
quq99 <eequq99@gmail.com>
2018-11-25 12:29:58 -0500

    add decide which bleu

--- metrics/bleu.py
+++ metrics/bleu.py
@@ -6,3 +6,13 @@ from nltk.translate.bleu_score import sentence_bleu
 
 def cal_bleu(hypothesis, references, weights, n_gram, individual_or_cumulative):
 
+
+def decide_which_bleu(hypothesis, n_gram=4,individual_or_cumulative='cumulative'):
+    if len(hypothesis)<n_gram:
+        print('error, length of hypothesis small than n_gram')
+        return 0
+
+    weights1 = [(1,0,0,0)]
+    if individual_or_cumulative=='cumulative':
+        if len(hypothesis)>=4:
+            return (1.0/4, 1.0/4, 1.0/4, 1.0/4)
