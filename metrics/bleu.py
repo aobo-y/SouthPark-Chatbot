6bed1c03f38923f0b8cada80a6ffa1b06151e2aa
quq99 <eequq99@gmail.com>
2018-11-25 17:38:14 -0500

    update bleu.py

--- metrics/bleu.py
+++ metrics/bleu.py
@@ -10,14 +10,15 @@ def cal_bleu(hypothesis, references, weights, n_gram, individual_or_cumulative):
 def decide_which_bleu(hypothesis, n_gram=4,individual_or_cumulative='cumulative'):
     if len(hypothesis)==0:
         print('error, length of hypothesis is zero')
-        return 0
+        return -2
     if n_gram > 4:
         print('error, we think n=4 is enough, chose n_gram<=4')
-        return 0
+        return -1
 
     if len(hypothesis)<n_gram:
         print('error, length of hypothesis small than n_gram')
-        n_gram = len(hypothesis)
+        #n_gram = len(hypothesis)
+        return 0
 
     weights1 = [(1,0,0,0), (0.5,0.5,0,0), (1.0/3,1.0/3,1.0/3,0), (0.25,0.25,0.25,0.25)]
     weights2 = [(1,0,0,0), (0,1,0,0), (0,0,1,0), (0,0,0,1)]
@@ -25,4 +26,4 @@ def decide_which_bleu(hypothesis, n_gram=4,individual_or_cumulative='cumulative'
         if len(hypothesis)>=4:
             return weights1[n_gram-1]
         elif len(hypothesis)==3:
-
+            return weights1[]
