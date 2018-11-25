ed0ba4d579723e3492ebd24ad87bcb7157d090d3
quq99 <eequq99@gmail.com>
2018-11-25 12:32:50 -0500

    add weights to bleu

--- metrics/bleu.py
+++ metrics/bleu.py
@@ -12,7 +12,7 @@ def decide_which_bleu(hypothesis, n_gram=4,individual_or_cumulative='cumulative'
         print('error, length of hypothesis small than n_gram')
         return 0
 
-    weights1 = [(1,0,0,0)]
+    weights1 = [(1,0,0,0), (0.5,0.5,0,0), (1.0/3,1.0/3,1.0/3,0), (0.25,0.25,0.25,0.25)]
     if individual_or_cumulative=='cumulative':
         if len(hypothesis)>=4:
             return (1.0/4, 1.0/4, 1.0/4, 1.0/4)
