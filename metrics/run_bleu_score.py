7108e66cbdb7c207fb18c4a37548dc0d391940e8
quq99 <eequq99@gmail.com>
2018-11-26 12:24:10 -0500

    update config.json run function

--- metrics/run_bleu_score.py
+++ metrics/run_bleu_score.py
@@ -1,6 +1,7 @@
 from six import text_type
 from nltk.tokenize import word_tokenize
 import string
+import json
 
 def make_tokens(sentence):
     # tokenize
@@ -14,4 +15,17 @@ def make_tokens(sentence):
     words = [word for word in stripped if word.isalpha()]
     return words
 
+def read_config(file_path):
+    json_ob = json.load(open(file_path,'r'))
+    return json_ob
 
+def run(config):
+    #
+    type_seq2seq = config['type']
+    # read file
+    r = open(config[])
+
+
+if __name__ == '__main__':
+    config = read_config('config.json')
+    run(config)
