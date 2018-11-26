953e8170b52bea1037df60f248ef4ce8aea9884d
quq99 <eequq99@gmail.com>
2018-11-25 19:28:56 -0500

    add test decide_which_bleu

--- metrics/bleu.py
+++ metrics/bleu.py
@@ -5,9 +5,11 @@ from nltk.translate.bleu_score import sentence_bleu
 
 
 def cal_bleu(hypothesis, references, weights, n_gram, individual_or_cumulative):
+    pass
 
-
-def decide_which_bleu(hypothesis, n_gram=4,individual_or_cumulative='cumulative'):
+def decide_which_bleu(hypothesis, \
+                      n_gram=4, \
+                      individual_or_cumulative='cumulative'):
     '''
     individual_or_cumulative==['cumulative','individual'] chose one of two choices.
     '''
@@ -27,4 +29,18 @@ def decide_which_bleu(hypothesis, n_gram=4,individual_or_cumulative='cumulative'
     weights2 = [(1,0,0,0), (0,1,0,0), (0,0,1,0), (0,0,0,1)]
     if individual_or_cumulative=='cumulative':
         return weights1[n_gram-1]
-    elif individual_or_cumulative=='individual'
+    elif individual_or_cumulative=='individual':
+        return weights2[n_gram-1]
+
+
+if __name__ == "__main__":
+    h1=['hello','world']
+    r1=['hello','world']
+    h2=['this', 'is', 'small', 'test']
+    r2=['this', 'is', 'a', 'test']
+    print(decide_which_bleu(h1, 1, 'individual'))
+    print(decide_which_bleu(h1, 1, ))
+    print(decide_which_bleu(h1, 2, 'individual'))
+    print(decide_which_bleu(h1, 2, ))
+    print(decide_which_bleu(h1, 3, 'individual'))
+    print(decide_which_bleu(h1, 3, ))
