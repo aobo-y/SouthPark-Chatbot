a172e159da9e072b3bab1e5dfe34f792d91ac850
Aobo Yang <yangaobo@gmail.com>
2018-12-13 01:30:16 -0500

    use model forward in greedy search

--- src/search_decoder.py
+++ src/search_decoder.py
@@ -31,32 +31,19 @@ class GreedySearchDecoder(nn.Module):
         self.model = model
 
     def forward(self, input_seq, input_length, speaker_id, sos, eos):
-        # Forward input through encoder model
-        encoder_outputs, encoder_hidden = self.model.encoder(input_seq, input_length)
-        # Prepare encoder's final hidden layer to be first hidden input to the decoder
-        decoder_hidden = self.model.cvt_hidden(encoder_hidden)
+        target_var = torch.full((1, 1), sos, dtype=torch.long, device=device)
 
-        # Initialize decoder input with SOS_token
-        decoder_input = torch.ones(1, 1, device=device, dtype=torch.long) * sos
-        # Initialize tensors to append decoded words to
-        all_tokens = torch.zeros([0], device=device, dtype=torch.long)
-        all_scores = torch.zeros([0], device=device)
-        # Iteratively decode one word token at a time
-        for _ in range(config.MAX_LENGTH):
-            # Forward pass through decoder
-            # Transform speaker_id from int into tensor with shape=(1, 1)
-            speaker_input = torch.LongTensor([[speaker_id]])
-            speaker_input = speaker_input.to(device)
-            decoder_output, decoder_hidden = self.model.decoder(decoder_input, speaker_input, decoder_hidden, encoder_outputs)
-            # Obtain most likely word token and its softmax score
-            decoder_scores, decoder_input = torch.max(decoder_output, dim=1)
-            # Record token and score
-            all_tokens = torch.cat((all_tokens, decoder_input), dim=0)
-            all_scores = torch.cat((all_scores, decoder_scores), dim=0)
-            # Prepare current token to be next decoder input (add a dimension)
-            decoder_input = torch.unsqueeze(decoder_input, 0)
-        # Returen collections of word tokens and scores
-        return all_tokens, all_scores
+        speaker_var = torch.LongTensor([[speaker_id]], device=device)
+
+        output_var = self.model(input_seq, input_length, target_var, speaker_var, config.MAX_LENGTH)
+
+        # squeeze batch dim
+        output_var = output_var.squeeze(1)
+
+        # Obtain most likely word token and its softmax score
+        scores, tokens = torch.max(output_var, dim=1)
+
+        return tokens, scores
 
 class BeamSearchDecoder(nn.Module):
     """
